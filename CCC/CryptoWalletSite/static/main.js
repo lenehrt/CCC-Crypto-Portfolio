e5c5621a8823d4d67a04a8ac4cecefd9d1029777
lenehrt <leonardo@lenehrt.com>
2021-12-10 19:36:14 -0500

    in progress

--- CCC/CryptoWalletSite/static/main.js
+++ CCC/CryptoWalletSite/static/main.js
@@ -5,186 +5,339 @@ const vm = new Vue({
     data: {
         walletAddress: '',
         zapperAPIkey : '96e0cc51-a62e-42ca-acee-910ea7d2a241',
+        listOfsupporteProtocals : [],
+        stakedCoins : [],
+        walletCoins : [],
+        netWorth: 0,
+
+        // List of coins in the wallet
+
+        // EthereumCoins : '',
+        // PolygonCoins : '',
+        // OptimismCoins: '',
+        // XdaiCoins : '',
+        // BinanceCoins : '',
+        // FantomCoins : '',
+        // AvalancheCoins : '',
+        // ArbitrumCoins : '',
+        // CeloCoins : '',
+        // HarmonyCoins : '',
         
-        EthereumCoins : '',
-        PolygonCoins : '',
-        OptimismCoins: '',
-        XdaiCoins : '',
-        BinanceCoins : '',
-        FantomCoins : '',
-        AvalancheCoins : '',
-        ArbitrumCoins : '',
-        CeloCoins : '',
-        HarmonyCoins : '',
+        // list of coins in vauls
+
     },
     methods: {
         
-        loadWallet: function() {
-            
+        getWallet: function() {
             ethereum.request({ method: 'eth_requestAccounts' }).then(response=>{ 
                 this.walletAddress = response
-                this.loadCoins()
+                this.checkProtocal()
             })
         },
-        loadCoins: function() {
-            
-            //Ethereum Network
+        checkProtocal: function() {
             axios({
-            method: 'get',
-            url: 'https://api.zapper.fi/v1/protocols/tokens/balances',
-            params: {
-                addresses : this.walletAddress,
-                network : 'ethereum',
-                api_key: '96e0cc51-a62e-42ca-acee-910ea7d2a241',
+                method: 'get',
+                url: 'https://api.zapper.fi/v1/protocols/balances/supported',
+                params: {
+                    addresses : this.walletAddress,
+                    api_key: '96e0cc51-a62e-42ca-acee-910ea7d2a241',
                 },
                 }).then(response => {
+                    try {
+
                     
-                    this.EthereumCoins = response.data[ this.walletAddress[0] ].products[0].assets
-                
-                }),  
+                        let listOfProtocals = response.data
+                        listOfProtocals.forEach( protocal => {
+                        
+                            let network = protocal.network
+                            let each = protocal.apps
 
+                                each.forEach(appObject => {
+                                    
+                                    let app = appObject.appId
 
-            //Polygon Network
-            axios({
-            method: 'get',
-            url: 'https://api.zapper.fi/v1/protocols/tokens/balances',
-            params: {
-                addresses : this.walletAddress,
-                network : 'polygon',
-                api_key: '96e0cc51-a62e-42ca-acee-910ea7d2a241',
-            },
-            }).then(response => {
-                
-                this.PolygonCoins = response.data[ this.walletAddress[0] ].products[0].assets
-            
-            }),
-            
-            //Optimism Network
-            axios({
-            method: 'get',
-            url: 'https://api.zapper.fi/v1/protocols/tokens/balances',
-            params: {
-                addresses : this.walletAddress,
-                network : 'optimism',
-                api_key: '96e0cc51-a62e-42ca-acee-910ea7d2a241',
-            },
-            }).then(response => {
-                
-                this.OptimismCoins = response.data[ this.walletAddress[0] ].products[0].assets
-            
-            }),
+                                    if ( app != 'tokens') {
 
-            //Xdai Network
-            axios({
-            method: 'get',
-            url: 'https://api.zapper.fi/v1/protocols/tokens/balances',
-            params: {
-                addresses : this.walletAddress,
-                network : 'xdai',
-                api_key: '96e0cc51-a62e-42ca-acee-910ea7d2a241',
-            },
-            }).then(response => {
-                
-                this.XdaiCoins = response.data[ this.walletAddress[0] ].products[0].assets
-            
-            }),
+                                        this.listOfsupporteProtocals.push({
 
-            //Binance Smart Chain Network
-            axios({
-            method: 'get',
-            url: 'https://api.zapper.fi/v1/protocols/tokens/balances',
-            params: {
-                addresses : this.walletAddress,
-                network : 'binance-smart-chain',
-                api_key: '96e0cc51-a62e-42ca-acee-910ea7d2a241',
-            },
-            }).then(response => {
-                
-                this.BinanceCoins = response.data[ this.walletAddress[0] ].products[0].assets
+                                            'network' : network,
+                                            'app' : app,
+                                            
+                                        })
+                                    }
+                                })
+                        })
+                    } catch (error) {}
+                    this.getWalletBalance()
+                    this.getStakedBalance()
+                })
+        },
+        getWalletBalance: function() {
             
-            }),
+            this.listOfsupporteProtocals.forEach( protocal => {
+                axios({
+                    method: 'get',
+                    url: 'https://api.zapper.fi/v1/protocols/tokens/balances',
+                    params: {
+                        addresses : this.walletAddress,
+                        network : protocal.network,
+                        api_key: '96e0cc51-a62e-42ca-acee-910ea7d2a241',
+                        },
+                        }).then(response => {
+                        try {
+                            let walletCoin = response.data[ this.walletAddress[0] ].products[0].assets
+                            
+                            
+                            if (walletCoin.length > 1) {
+                                walletCoin.forEach( obj => {
+                                    
+                                    if (!obj.symbol.startsWith('s')) {
+                                        this.walletCoins.push(obj)
+                                    }
+                            
+                                })
+                            }
+                            else {
+                                if (!walletCoin[0].symbol.startsWith('s')) {
+                                    
+                                    this.walletCoins.push(walletCoin[0])
+                                }
+                            }
 
-            //Fantom Network
-            axios({
-            method: 'get',
-            url: 'https://api.zapper.fi/v1/protocols/tokens/balances',
-            params: {
-                addresses : this.walletAddress,
-                network : 'fantom',
-                api_key: '96e0cc51-a62e-42ca-acee-910ea7d2a241',
-                },
-                }).then(response => {
+                        } catch (error) {}
+                        })
+            })
+
+        },
+        getStakedBalance: function() {
+            
+            this.listOfsupporteProtocals.forEach( protocal => {
+                
+                axios({
+                    method: 'get',
+                    url: `https://api.zapper.fi/v1/protocols/${protocal.app}/balances`,
+                    params: {
+                        addresses : this.walletAddress,
+                        network : protocal.network,
+                        api_key: '96e0cc51-a62e-42ca-acee-910ea7d2a241',
+                    },
+                    }).then(response => {               
+                        try {
+                        let vaultCoin = response.data[ this.walletAddress[0] ].products[0].assets[0]
+                        this.stakedCoins.push(vaultCoin) }
+                        catch (error) {}
                     
-                    this.FantomCoins = response.data[ this.walletAddress[0] ].products[0].assets
-                
-                }),  
+                    })
+            })
+        },
+        calNetWorth: function() {
+            console.log(this.netWorth)
 
-            //Avalanche Network
-            axios({
-            method: 'get',
-            url: 'https://api.zapper.fi/v1/protocols/tokens/balances',
-            params: {
-                addresses : this.walletAddress,
-                network : 'avalanche',
-                api_key: '96e0cc51-a62e-42ca-acee-910ea7d2a241',
-            },
-            }).then(response => {
-                
-                this.AvalancheCoins = response.data[ this.walletAddress[0] ].products[0].assets
+            this.walletCoins.forEach( value => {
+                this.netWorth += value.balanceUSD
+            })
+            this.stakedCoins.forEach( value => {
+                this.netWorth += value.balanceUSD
+            })
+        },
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+        // loadCoins: function() {
             
-            }),
+        //     //Ethereum Network
+        //     axios({
+        //     method: 'get',
+        //     url: 'https://api.zapper.fi/v1/protocols/tokens/balances',
+        //     params: {
+        //         addresses : this.walletAddress,
+        //         network : 'ethereum',
+        //         api_key: '96e0cc51-a62e-42ca-acee-910ea7d2a241',
+        //         },
+        //         }).then(response => {
+        //         try {
+        //             this.EthereumCoins = response.data[ this.walletAddress[0] ].products[0].assets
+        //         } catch (error) {}
+        //         }),  
+
+
+        //     //Polygon Network
+        //     axios({
+        //     method: 'get',
+        //     url: 'https://api.zapper.fi/v1/protocols/tokens/balances',
+        //     params: {
+        //         addresses : this.walletAddress,
+        //         network : 'polygon',
+        //         api_key: '96e0cc51-a62e-42ca-acee-910ea7d2a241',
+        //     },
+        //     }).then(response => {
+        //         try {
+        //             this.PolygonCoins = response.data[ this.walletAddress[0] ].products[0].assets
+        //         } catch (error) {}
+        //     }),
             
-            //Arbitrum Network
-            axios({
-            method: 'get',
-            url: 'https://api.zapper.fi/v1/protocols/tokens/balances',
-            params: {
-                addresses : this.walletAddress,
-                network : 'arbitrum',
-                api_key: '96e0cc51-a62e-42ca-acee-910ea7d2a241',
-            },
-            }).then(response => {
-                
-                this.ArbitrumCoins = rresponse.data[ this.walletAddress[0] ].products[0].assets
+        //     //Optimism Network
+        //     axios({
+        //     method: 'get',
+        //     url: 'https://api.zapper.fi/v1/protocols/tokens/balances',
+        //     params: {
+        //         addresses : this.walletAddress,
+        //         network : 'optimism',
+        //         api_key: '96e0cc51-a62e-42ca-acee-910ea7d2a241',
+        //     },
+        //     }).then(response => {
+        //         try {
+        //             this.OptimismCoins = response.data[ this.walletAddress[0] ].products[0].assets
+        //         } catch (error) {}    
+        //     }),
+
+        //     //Xdai Network
+        //     axios({
+        //     method: 'get',
+        //     url: 'https://api.zapper.fi/v1/protocols/tokens/balances',
+        //     params: {
+        //         addresses : this.walletAddress,
+        //         network : 'xdai',
+        //         api_key: '96e0cc51-a62e-42ca-acee-910ea7d2a241',
+        //     },
+        //     }).then(response => {
+        //         try {
+        //             this.XdaiCoins = response.data[ this.walletAddress[0] ].products[0].assets
+        //         } catch (error) {}
+        //     }),
+
+        //     //Binance Smart Chain Network
+        //     axios({
+        //     method: 'get',
+        //     url: 'https://api.zapper.fi/v1/protocols/tokens/balances',
+        //     params: {
+        //         addresses : this.walletAddress,
+        //         network : 'binance-smart-chain',
+        //         api_key: '96e0cc51-a62e-42ca-acee-910ea7d2a241',
+        //     },
+        //     }).then(response => {
+        //         try {
+        //             this.BinanceCoins = response.data[ this.walletAddress[0] ].products[0].assets
+        //         } catch (error) {}
+        //     }),
+
+        //     //Fantom Network
+        //     axios({
+        //     method: 'get',
+        //     url: 'https://api.zapper.fi/v1/protocols/tokens/balances',
+        //     params: {
+        //         addresses : this.walletAddress,
+        //         network : 'fantom',
+        //         api_key: '96e0cc51-a62e-42ca-acee-910ea7d2a241',
+        //         },
+        //         }).then(response => {
+        //             try {
+        //                 this.FantomCoins = response.data[ this.walletAddress[0] ].products[0].assets
+        //             } catch (error) {}
+        //         }),  
+
+        //     //Avalanche Network
+        //     axios({
+        //     method: 'get',
+        //     url: 'https://api.zapper.fi/v1/protocols/tokens/balances',
+        //     params: {
+        //         addresses : this.walletAddress,
+        //         network : 'avalanche',
+        //         api_key: '96e0cc51-a62e-42ca-acee-910ea7d2a241',
+        //     },
+        //     }).then(response => {
+        //         try {
+        //             this.AvalancheCoins = response.data[ this.walletAddress[0] ].products[0].assets
+        //         } catch (error) {}         
+        //     }),
             
-            }),
+        //     //Arbitrum Network
+        //     axios({
+        //     method: 'get',
+        //     url: 'https://api.zapper.fi/v1/protocols/tokens/balances',
+        //     params: {
+        //         addresses : this.walletAddress,
+        //         network : 'arbitrum',
+        //         api_key: '96e0cc51-a62e-42ca-acee-910ea7d2a241',
+        //     },
+        //     }).then(response => {
+        //         try {
+        //             this.ArbitrumCoins = response.data[ this.walletAddress[0] ].products[0].assets
+        //         } catch (error) {} 
+        //     }),
 
-            //Celo Network
-            axios({
-            method: 'get',
-            url: 'https://api.zapper.fi/v1/protocols/tokens/balances',
-            params: {
-                addresses : this.walletAddress,
-                network : 'celo',
-                api_key: '96e0cc51-a62e-42ca-acee-910ea7d2a241',
-            },
-            }).then(response => {
-                
-                this.CeloCoins = response.data[ this.walletAddress[0] ].products[0].assets
-            
-            }),
+        //     //Celo Network
+        //     axios({
+        //     method: 'get',
+        //     url: 'https://api.zapper.fi/v1/protocols/tokens/balances',
+        //     params: {
+        //         addresses : this.walletAddress,
+        //         network : 'celo',
+        //         api_key: '96e0cc51-a62e-42ca-acee-910ea7d2a241',
+        //     },
+        //     }).then(response => {
+        //         try {
+        //             this.CeloCoins = response.data[ this.walletAddress[0] ].products[0].assets
+        //         } catch (error) {} 
+        //     }),
 
-            //Harmony Network
-            axios({
-            method: 'get',
-            url: 'https://api.zapper.fi/v1/protocols/tokens/balances',
-            params: {
-                addresses : this.walletAddress,
-                network : 'harmony',
-                api_key: '96e0cc51-a62e-42ca-acee-910ea7d2a241',
-            },
-            }).then(response => {
-                
-                this.HarmonyCoins = response.data[ this.walletAddress[0] ].products[0].assets
-            
-            }) 
+        //     //Harmony Network
+        //     axios({
+        //     method: 'get',
+        //     url: 'https://api.zapper.fi/v1/protocols/tokens/balances',
+        //     params: {
+        //         addresses : this.walletAddress,
+        //         network : 'harmony',
+        //         api_key: '96e0cc51-a62e-42ca-acee-910ea7d2a241',
+        //     },
+        //     }).then(response => {
+        //         try {
+        //             this.HarmonyCoins = response.data[ this.walletAddress[0] ].products[0].assets
+        //         } catch (error) {} 
+        //     }) 
 
-            }
-        }
-})
+        // },
 
 
 
+        // getListOfProtocals: function(listOfProtocals) {
+        //     listOfProtocals.forEach( protocal => {
+        //         let network = protocal.network
+        //         let each = protocal.apps
+        //         each.forEach(appObject => {
+        //             let app = appObject.appId
+        //             if ( app != 'tokens') {
+        //                 this.listOfsupporteProtocals.push({
+        //                     'network' : network,
+        //                     'app' : app
+        //                 })
+        //             }
+        //         })
+        //     } )
+        // },
 
 
     // loadWallet: function() {
@@ -201,3 +354,6 @@ const vm = new Vue({
     //             }, false);
     //     })
     // },
+
+    }
+})
